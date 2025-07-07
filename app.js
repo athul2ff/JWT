010cf1a38dc0b59449c962aa896cb2662ccdcac1
athul2ff <athul2ff@gmail.com>
2025-07-07 19:24:53 +0530

    add mongoose && model

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 const app = express();
+
+connectDB();
 
 app.get('/',(req,res)=>{
     console.log(req);
     res.send("hello")
 })
 
-
-
-
-
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT,()=>console.log(`app is listening port ${PORT}`));
--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connectDB = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("db connected");
-   
-    } catch (error) {
-        console.log(error.message);  
-        process.exit(1);          
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("db connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
